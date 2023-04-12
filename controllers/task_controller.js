796a6257eafa8df020ecd77009c84995976c1501
Joshua Onwuemene <emjay6299@gmail.com>
2023-04-12 15:27:01 +0200

    clean up task_controller

--- controllers/task_controller.js
+++ controllers/task_controller.js
@@ -1,3 +1,4 @@
+// import dependencies/modules
 const { checkToken } = require("../middlewares/checkDecodedToken")
 const Task = require("../models/Tasks")
 const formData = require('form-data');
@@ -5,12 +6,11 @@ const Mailgun = require('mailgun.js');
 const { DOMAIN } = process.env
 const { api_key } = process.env
 
-// Create Task
+// Create new Task function
 const createTask = async (req, res) => {
   // use middleware to decode Token
   // populate the task author field with signed in user id from decoded token
     const decodedToken = checkToken(req.cookies.jwt)
-
     const { taskName, completed } = req.body;
 
     const task = new Task({ 
@@ -20,96 +20,78 @@ const createTask = async (req, res) => {
       completed 
     } )
 
-    await task.save();
-                    
+    await task.save();      
     res.status(200).json({ task })
 }
 
 
-// Get all Tasks
+// Get user's Tasks function
 const getAllTasks = async (req, res) => {
-
     const decodedToken = checkToken(req.cookies.jwt)
-
     const tasks = await Task.find({ author: decodedToken.id }).populate("author", "firstName lastName email") 
-      // ("field", "keys to return")
-      // {path: "author", model: "Users" }
-
+      // ("field", "keys to return") || .populate ({path: "author", model: "Users" })
     res.status(200).json({ 
       tasks, 
       Task_amount: tasks.length })
   }
 
 
-// Get a Single Task
+// Get a single Task function
 const getSingleTask = async (req, res) => {
-
     const taskID = { _id: req.params.id }
-
     const task = await Task.findOne(taskID).populate("author", "firstName lastName email")
-
     res.status(200).json({ task })
   }
 
 
-// Update Tasks
+// Update a Task function
 const updateTask =  async (req, res) => {
-
     const taskID = { _id: req.params.id }
-
     const task = await Task.findOneAndUpdate(taskID, req.body, { new: true, runValidators: true })
-
     res.status(200).json({ task })
 }
 
 
-// Delete Tasks
+// Delete a Task function
 const deleteTask =  async (req, res) => {
-
     const taskID = { _id: req.params.id }
-
-     await Task.findOneAndDelete(taskID)
-    
-    res.status(200)
-      .json({ message: "Success! Task has been removed" })
+    await Task.findOneAndDelete(taskID)
+    res.status(200).json({ message: "Success! Task has been removed" })
 }
 
 
+//send list to email function
 const sendListToemail = async (req, res) => {
   try {
-		const {email} = req.body;
-
-    const decodedToken = checkToken(req.cookies.jwt)
-
-    const task = await Task.find({ author: decodedToken.id })
-      .select("taskName completed")
-
-   const text = task.map(obj => {
-      return `TaskName: ${obj.taskName}, Completed: ${obj.completed}\n`
-    }).join("")
+	const {email} = req.body;
+    	const decodedToken = checkToken(req.cookies.jwt)
+    	const task = await Task.find({ author: decodedToken.id }.select("taskName completed")
+				     
+	//define email text
+   	const text = task.map(obj => {
+      		return `TaskName: ${obj.taskName}, Completed: ${obj.completed}\n`
+    	}).join("")
   
-    // send email containing list of task
-    const mailgun = new Mailgun(formData);
+	// send email containing list of task
+	const mailgun = new Mailgun(formData);
+	
+	const client = mailgun.client({
+		username: 'api', 
+		key: api_key
+	});
 
-		const client = mailgun.client({
-			username: 'api', 
-			key: api_key
-		});
+	const messageData = {
+		from: 'joshua Onwuemene <josh@mailgun.org>',
+		to: email,
+		subject: 'REQUEST FOR LIST OF TASKS',
+		text: `Here is a list of your tasks\n \n ${text}`
+	}
 
-		const messageData = {
-			from: 'joshua Onwuemene <josh@mailgun.org>',
-			to: email,
-			subject: 'REQUEST FOR LIST OF TASKS',
-			text: `Here is a list of your tasks\n 
-        ${text}`
-    }
+	client.messages.create(DOMAIN, messageData)
+      	.then( res => console.log(res) )
+      	.catch( err => console.error(err) )
 
-		client.messages.create(DOMAIN, messageData)
-      .then( res => console.log(res) )
-      .catch( err => console.error(err) )
-
-    res.status(200).json(messageData)
-
+    	res.status(200).json(messageData);
     } 
     catch (err) {
         console.log(err);
