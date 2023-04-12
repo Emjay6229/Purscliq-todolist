b22e2419d9c243c09b6944c3f834ee2ae6385170
Joshua Onwuemene <emjay6299@gmail.com>
2023-04-12 15:30:39 +0200

    Update auth_controller.js

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -1,3 +1,4 @@
+// import modules/ dependencies
 require("dotenv").config()
 const User = require("../models/Users")
 const bcrypt = require("bcrypt")
@@ -5,16 +6,15 @@ const jwt = require("jsonwebtoken")
 const { secret_key } = process.env 
 const jwt_life = 3600 * 1000
 
+// sign Jwt function
 const createToken = (id, email) => {
     const payLoad = { id, email }
     return jwt.sign( payLoad, secret_key, { expiresIn: jwt_life })
 }
 
-
 // REGISTER NEW USER
 const userSignup = async ( req, res ) => {
     const { firstName, lastName, email, password } = req.body;
-
     const securePass = await bcrypt.hash(password, bcrypt.genSaltSync(10))
 
     const user = new User( {
@@ -26,15 +26,9 @@ const userSignup = async ( req, res ) => {
     } )
 
     await user.save();
-
-    const token = createToken(user._id, user.email);
+    const token = createToken( user._id, user.email );
     
-    res.cookie("jwt", token, { 
-        httpOnly: true, 
-        maxAge: jwt_life
-    })
-        .status(200)
-        .json({ 
+    res.cookie( "jwt", token, { httpOnly: true, maxAge: jwt_life } ).status(200).json({ 
             id: user._id, 
             email: user.email,
             password: user.password
@@ -46,11 +40,8 @@ const userSignup = async ( req, res ) => {
 
 // USER LOGIN
 const userSignin = async (req, res) => {
-
     const { email, password } = req.body;
-
     const existingUser = await User.login(email, password)
-
     const token = createToken( existingUser._id, existingUser.email );
 
     res.cookie("jwt", token, { 
@@ -70,23 +61,16 @@ const userSignin = async (req, res) => {
 
 // UPDATE USER
 const updateUser = async (req, res) => {
-
     const userID = { _id: req.params.id };
-
-    const updatedUser = await User.findOneAndUpdate(userID, req.body, { new: true, runValidators: true})
-            .select("_id email password")
-
+    const updatedUser = await User.findOneAndUpdate(userID, req.body, { new: true, runValidators: true}).select("_id email password")
     res.status(200).json( updatedUser ); 
 }
 
 
 // DELETE USER
 const deleteUser = async (req, res) => {
-
     const userID = { _id: req.params.id };
-
     await User.findOneAndDelete(userID);
-
     res.status(200).json( {
         Success: "Your account is deleted"
     })
@@ -94,11 +78,8 @@ const deleteUser = async (req, res) => {
 
 // USER LOGOUT
 const userSignout = (req, res) => {
-
-    res.cookie("jwt", "", { httpOnly: true, maxAge: 1 })
-        .status(200)
-        .json ({ Success: "You have been succesfully Logged out" })
-    }
+    res.cookie("jwt", "", { httpOnly: true, maxAge: 1 }).status(200).json ({ Success: "You have been succesfully Logged out" })
+  }
 
 module.exports = {
     userSignup, 
