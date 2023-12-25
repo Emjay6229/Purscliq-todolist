5e8ad6d3b988d7e5e361889bfe8e54f1cf35c4c3
Joshua Onwuemene <emjay6299@gmail.com>
2023-12-25 10:31:37 +0100

    Update Tasks.js

--- models/Tasks.js
+++ models/Tasks.js
@@ -3,34 +3,26 @@ const { Schema } = mongoose;
 const { formatDateToCustomFormat } = require("../controllers/utils/dateUtil");
 
 const taskSchema = new Schema(
-    {
-        title: { type: String, required: [ true, "Enter this field to add a task"] }, 
+	{
+		title: { type: String, required: [ true, "Enter this field to add a task"] }, 
+		createdBy: { type: Schema.Types.ObjectId, ref: "Users", required: true },
+		category: { type: String, required: true },
+		description: String,
+		startDate: { type: String, default: formatDateToCustomFormat() },
+		endDate: String,
+		status: {
+			type: String,
+			enum: { 
+					values: ['completed', 'pending', 'in progress'], 
+					message: '{VALUE} is not supported' 
+				},
+				default: 'in progress'
+			},
 
-        createdBy: { type: Schema.Types.ObjectId, ref: "Users", required: true },
-
-        category: { type: String, required: true },
-
-        description: String,
-
-        startDate: { type: String, default: formatDateToCustomFormat() },
-
-        endDate: String,
-
-        status: {
-            type: String,
-            enum: { 
-                values: ['completed', 'pending', 'in progress'], 
-                message: '{VALUE} is not supported' 
-            },
-            default: 'in progress'
-        },
-
-        from: { type: String, ref: "Users" },
-
-        to: { type: String, ref: "Users" },
-
-        createdAt: { type: String, default: formatDateToCustomFormat() }
-    }
+			from: { type: String, ref: "Users" },
+			to: { type: String, ref: "Users" },
+			createdAt: { type: String, default: formatDateToCustomFormat() }
+	}
 )
 
 taskSchema.index( {title: "text", description: "text"});
