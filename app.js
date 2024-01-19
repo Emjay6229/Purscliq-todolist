249c4711a016dc994119b130b9e93c031f0f78c1
Joshua Onwuemene <emjay6299@gmail.com>
2024-01-19 13:50:10 +0100

    Update app.js: disable cors setup

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
-const cors = require("cors");
+// const cors = require("cors");
 
 const { verifyToken } = require("./middlewares/auth");
 
@@ -15,16 +15,8 @@ const app = express();
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: false }));
 app.use(cookieParser());
-
-app.use(cors());
-
-// app.use(
-//   cors({
-//     origin: [
-//       "https://task-management-nu-nine.vercel.app", 
-//       "http://localhost:5173", 
-//       "http://localhost:5174"
-//     ],
+// app.use(cors({
+//     origin: ["https://task-management-nu-nine.vercel.app"],
 //     methods: "GET, HEAD, PUT, POST, PATCH, DELETE",
 //     credentials: true
 //   })
